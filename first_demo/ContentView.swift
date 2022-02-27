f89fd2b6393274a5867f4e8e94bd0fc6818f9fc1
lijiaqi <450103187@qq.com>
2022-02-27 17:30:10 +0800

    first commit

---
+++ first_demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  first_demo
+//
+//  Created by 李佳奇 on 2022/2/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ first_demo/first_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  first_demoApp.swift
+//  first_demo
+//
+//  Created by 李佳奇 on 2022/2/27.
+//
+
+import SwiftUI
+
+@main
+struct first_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
